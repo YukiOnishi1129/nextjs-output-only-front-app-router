0c331fbf1a2e132fa8cb2069c8e017459181a445
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-17 16:37:36 +0900

    atoms

---
+++ src/components/atoms/NavigationLink/NavigationLink.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import Link from "next/link";
+import styles from "./style.module.css";
+
+type NavigationLinkProps = {
+  title: string;
+  linkPath: string;
+};
+
+export const NavigationLink: FC<NavigationLinkProps> = ({
+  title,
+  linkPath,
+}) => (
+  <li className={styles.li}>
+    <Link href={linkPath}>{title}</Link>
+  </li>
+);
