dab33c44bbe84264cdbad0fadd9a4719c8cdd652
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-17 16:55:33 +0900

    template

---
+++ src/components/templates/TodoDetailTemplate/TodoDetailTemplate.tsx
@@ -0,0 +1,26 @@
+import { useParams } from "next/navigation";
+import { useTodoContext } from "@/hooks/useTodoContext";
+import { BaseLayout } from "@/components/organisms";
+import { InputForm, TextArea } from "@/components/atoms";
+import styles from "./style.module.css";
+
+export const TodoDetailTemplate = () => {
+  const { originTodoList } = useTodoContext();
+  const { id } = useParams();
+  const todo = originTodoList.find((todo) => String(todo.id) === id);
+
+  return (
+    <BaseLayout title={"TodoDetail"}>
+      {!!todo && (
+        <div className={styles.container}>
+          <div className={styles.area}>
+            <InputForm disabled value={todo.title} placeholder={"Title"} />
+          </div>
+          <div className={styles.area}>
+            <TextArea disabled value={todo.content} placeholder={"Content"} />
+          </div>
+        </div>
+      )}
+    </BaseLayout>
+  );
+};
---
+++ src/components/templates/TodoListTemplate/TodoListTemplate.tsx
@@ -0,0 +1,59 @@
+import { useMemo } from "react";
+import { useForm, Controller } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InputFormSection } from "@/components/molecules";
+import { BaseLayout, TodoList } from "@/components/organisms";
+import { useTodoContext } from "@/hooks/useTodoContext";
+import styles from "./style.module.css";
+
+const schema = z.object({
+  keyword: z.string(),
+});
+
+export const TodoListTemplate = () => {
+  // コンテキストから状態とロジックを呼び出してコンポーネントにあてがう
+  const { originTodoList, deleteTodo } = useTodoContext();
+
+  const { control, watch } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: { keyword: "" },
+  });
+  const searchKeyword = watch("keyword");
+
+  /* 表示用TodoList */
+  const showTodoList = useMemo(() => {
+    const regexp = new RegExp("^" + searchKeyword, "i");
+    return originTodoList.filter((todo) => {
+      // 検索キーワードに部分一致したTodoだけを一覧表示する
+      return todo.title.match(regexp);
+    });
+    // useMemoの第二引数([originTodoList, searchKeyword])に依存して処理が実行される
+    // originTodoListとsearchKeywordの値が変更される度にfilterの検索処理が実行
+    // ただし結果が前回と同じならキャッシュを返却し処理は実行されない(無駄な処理を省いている)
+    // 詳しくはuseMemoを調べてください。
+  }, [originTodoList, searchKeyword]);
+
+  return (
+    <BaseLayout title={"TodoList"}>
+      <div className={styles.container}>
+        {/* Todo検索フォームエリア */}
+        <div className={styles.area}>
+          <Controller
+            name="keyword"
+            render={({ field }) => (
+              <InputFormSection placeholder={"Search Keyword"} {...field} />
+            )}
+            control={control}
+          />
+        </div>
+        {/* Todoリスト一覧表示 */}
+        <div className={styles.area}>
+          {showTodoList.length > 0 && (
+            <TodoList todoList={showTodoList} handleDeleteTodo={deleteTodo} />
+          )}
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
